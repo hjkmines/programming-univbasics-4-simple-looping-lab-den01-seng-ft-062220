e4234e7fb8a8e99571005eda03823b5262d0e67e
Hyung Kim <hjkmines@gmail.com>
2020-06-01 23:09:53 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,3 +15,24 @@ def loop_message_n_times (string, n)
   end  
   
 end
+
+def output_array(array)
+  
+  counter = 0 
+  
+  while counter < array.length 
+    puts array[counter]
+    counter += 1 
+  end 
+  
+end
+
+def return_string_array (array) 
+  
+  array.each do |x|
+    x.to_s
+  end 
+  
+  array
+  
+end 
