372e988d475552d94072f1223b02bd54e72e0ff7
Hyung Kim <hjkmines@gmail.com>
2020-06-01 23:24:24 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -29,11 +29,9 @@ def output_array(array)
 end
 
 def return_string_array (array) 
-  
-  counter = 0
-  while counter < array.length
-    array[counter].to_s   
-    counter += 1 
+  converted = []
+
+  array.each do {|x| 
   end 
   
 end 
