e928cb449af9233afc1e2be03876ff8d7df2bd40
Hyung Kim <hjkmines@gmail.com>
2020-06-01 23:05:07 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,16 @@
-# Write your methods here
+def loop_message_five_times (string)
+  
+  counter = 0 
+  
+  while counter < 5
+    puts string 
+    counter += 1
+  end 
+  
+
+def loop_message_n_times (string, n)
+
+  n.times do 
+    puts string
+  end  
+end
